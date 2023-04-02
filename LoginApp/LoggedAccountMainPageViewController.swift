bf7b198978891d8ad85b8c549a0568a1c6856786
Zaki <menza.zm@gmail.com>
2023-04-02 18:51:44 +0600

    Logic of interface

---
+++ LoginApp/LoggedAccountMainPageViewController.swift
@@ -0,0 +1,40 @@
+//
+//  LoggedAccountMainPageViewController.swift
+//  LoginApp
+//
+//  Created by Zaki on 02.04.2023.
+//
+
+import UIKit
+
+final class LoggedAccountMainPageViewController: UIViewController {
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var emojiLabel: UILabel!
+    
+    var name: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        emojiLabel.text = "\u{1F44B}"
+        nameLabel.text =  name
+        setUpBackgroundColor()
+    }
+    
+    private func setUpBackgroundColor() {
+        view.backgroundColor = .systemCyan
+        navigationItem.title = "Gradient View"
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.colors = [UIColor.systemPink.cgColor, UIColor.systemCyan.cgColor]
+        
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 0.0, y: 1.0)
+        
+        gradientLayer.frame = view.frame
+        
+        view.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+
+}
